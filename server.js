15e6dab1cea7677c9d7535204fff624ed50baaf0
anatsrii <anat.allsolution@gmail.comgit config user.name anatsriigit config user.email git config --global user.email anat.allsolution@gmail.com>
2022-09-22 19:43:14 +0700

    try ping pong message

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ function handleEvent(event) {
   } else if (event.type == 'message' || event.message.type == 'hello') {
 
     // // create a echoing text message
-    const echo = { type: 'text', text: 'hello' };
+    const echo = { type: 'text', text: `${event.message.type.text}` };
 
     // // use reply API
 
