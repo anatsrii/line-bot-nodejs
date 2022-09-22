6a144e354ccbf5822899e8066244f4796e407111
anatsrii <anat.allsolution@gmail.comgit config user.name anatsriigit config user.email git config --global user.email anat.allsolution@gmail.com>
2022-09-22 17:03:53 +0700

    update handle event filter handle message and type

--- server.js
+++ server.js
@@ -41,10 +41,11 @@ function handleEvent(event) {
   }
 
   // // create a echoing text message
-  // const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: event.message.text };
 
   // // use reply API
-  return client.replyMessage(event.replyToken,{ type: 'text', text: event.message.text });
+  
+  return client.replyMessage(event.replyToken, echo);
 }
 
 // listen on port
