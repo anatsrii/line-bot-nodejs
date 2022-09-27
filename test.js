9c204f23365a037f6b504c2743feec328b27f659
anatsrii <anat.allsolution@gmail.comgit config user.name anatsriigit config user.email git config --global user.email anat.allsolution@gmail.com>
2022-09-27 19:26:55 +0700

    add test file

---
+++ test.js
@@ -0,0 +1,11 @@
+const coins = require('./coins.json')
+
+// let result = coins.findIndex(checkName)
+function findArr () {
+  return 
+}
+let result = coins.filter((id) => {
+  return id === id.name
+})
+
+console.log(result)
