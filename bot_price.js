9546f786bf8fea23febaa86178ced603f4ab92b6
anatsrii <anat.allsolution@gmail.comgit config user.name anatsriigit config user.email git config --global user.email anat.allsolution@gmail.com>
2022-10-01 23:35:04 +0700

    add handler sticker, image function

--- bot_price.js
+++ bot_price.js
@@ -8,34 +8,46 @@ const client = new line.Client(config);
 
 const handlePrice = async function (event) {
   let tokens = event.replyToken;
-  // let currency = '/' + event.message.text;
+  let words = event.message.text;
+  let targetCurrency = words.slice(1, words.length);
+  let targetId = await coinName.filter( (idx) => idx.symbol === targetCurrency);
+  console.log(targetId)
   // ignore non-text-message event
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
-    // if (currency)
-  } else  {
+    // if (targetId)
+  } else if (targetId.length === 0) {
+    return client.replyMessage(tokens,{type: 'text', text: 'Invalid token symbol'})
+  } else {
     try {
-      let words = event.message.text;
-      let targetCurrency = words.slice(1, words.length);
-      console.log('function handlePrice',targetCurrency)
-      let url = `https://api.coingecko.com/api/v3/simple/price?ids=${targetCurrency}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`;
+      console.log(targetId[0].id)
+      let url = `https://api.coingecko.com/api/v3/simple/price?ids=${targetId[0].id}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`;
       
-      // let arr = []
       let response = await axios.get(url);
       // console.log(test)
       let price = Object.values(response.data);
-      let priceName = Object.keys(response.data);
+      let targetPrice = '$' + ' ' + price[0].usd;
+      let marketcap = '$' + ' ' + (parseFloat(price[0].usd_market_cap.toFixed(2)).toLocaleString('en'));
+      let volume = '$' + ' ' + parseFloat(price[0].usd_24h_vol.toFixed(2)).toLocaleString('en');
+      let percentChange = price[0].usd_24h_change.toFixed(2) + ' ' + '%';
       console.log(price)
       
       return client.replyMessage(tokens, {
-        type: 'text', text: `${priceName}`
+        type: 'text', 
+        text: ` Crypto Price Bot 
+        ${targetId[0].id} 
+        ${targetPrice}
+        Mcap: ${marketcap} 
+        Vol(24hr): ${volume} 
+        Change(24hr): ${percentChange}`
       })
+      
+     
       
     } catch (err) {
       console.error(err)
       return Promise.resolve(null);
     }
-    
   }
 }
 
--- server.js
+++ server.js
@@ -1,68 +1,66 @@
 const line = require('@line/bot-sdk');
+const config = require('./config');
 const express = require('express');
-const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const handleWords = require('./bot_words.js');
+const handlePrice = require('./bot_price')
+const stickerHandler = require('./bot_sticker');
+const handlerImage = require('./bot_image');
 
 
 // create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Loop events เพื่อส่งให้แต่ละ Function ไปทำงานต่อ
+app.post('/', line.middleware(config), async (req, res) => {
+  // รับ req.body
+  let events = req.body.events;
 
-// create LINE SDK config from env variables
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
-
-// create LINE SDK client
-const client = new line.Client(config);
-
-
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-// app.post('/', line.middleware(config), async (req, res) => {
-//   try {
-//     const events = req.body.events;
-//     console.log(`events = `, events);
-//     return events.lenght > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send("OK")
-//   } catch (error) {
-//     res.status(500).end();
-//   }
-// });
-app.post('/', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
+  console.log('sliceWord =', events[0])
+  // send to stickerhandler (ดัก sticker & image ) ถ้าจะขยายค่อยมา filter เพิ่มเติม
+  if (events[0].message.type === 'sticker') {
+    let result = await events.map(stickerHandler);
+    try {
+      res.json(result)
+    } catch (err) {
+      console.error(err)
+      res.status(500).end();
+    }
+    // send to handlerPrice 
+  } else if (events[0].message.type === 'image') {
+    let result = await events.map(handlerImage);
+    try {
+      res.json(result)
+    } catch (err) {
       console.error(err);
       res.status(500).end();
-    });
-    console.log(`events = `, req.body.events);
-});
-
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  } else if (event.type === 'message' && event.message.type.text == 'm') {
-    
-    // // create a echoing text message
-    const echo = { type: 'text', text: 'คนเจ้าชู้' };
-    
-    // // use reply API
-    
-    return client.replyMessage(event.replyToken, echo);
+    }
+  } else if (events[0].message.text.slice(0, 1) === '/') {
+    let result = await events.map(handlePrice);
+    try {
+      res.json(result)
+    } catch (err) {
+      console.error(err)
+      res.status(500).end();
+    }
+    // send to handler
+  } else if (events) {
+    let result = await events.map(handleWords);
+    try {
+      res.json(result)
+    } catch (err) {
+      console.error(err);
+      res.status(500).end();
+    }
   }
-  return Promise.resolve(null);
-
-}
+  res.status(500).end();
+})
 
 // listen on port
-const port = process.env.PORT ;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
